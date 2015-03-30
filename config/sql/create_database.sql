6b61d201b76854aa5b44d98523144c5f86709da7
liazhang <304957127@qq.com>
2015-03-30 13:39:48 +0800

    [zhang liang] update the field names and table names in create_database.sql file

--- config/sql/create_database.sql
+++ config/sql/create_database.sql
@@ -1,22 +1,22 @@
 --create database strike;
 
 
-create table t_person(id int  NOT NULL AUTO_INCREMENT, name char(50) not null, img_path varchar(100), primary key (id));
+    create table t_person(id int  NOT NULL AUTO_INCREMENT, name char(50) not null, img_path varchar(100), primary key (id));
 
-create table t_strike
-(
-     person_id int ,
-     strike_count int not null default 0,
-     star_count int not null default 0,
-     FOREIGN KEY (person_id) references t_person(id)
-);
+    create table t_strike
+    (
+         person_id int ,
+         strike_count int not null default 0,
+         star_count int not null default 0,
+         FOREIGN KEY (person_id) references t_person(id)
+    );
 
-insert into t_person(name, img_path) values ('Zhang Shen','img/baymax.jpg');
-insert into t_person(name, img_path) values ('Binfang Huang','img/xiaohuangren.jpeg');
-insert into t_person(name, img_path) values ('KK','img/maidou.jpg');
-insert into t_person(name, img_path) values ('Qiaorong Fan','img/huiguniang.jpg');
+    insert into t_person(name, img_path) values ('Zhang Shen','img/baymax.jpg');
+    insert into t_person(name, img_path) values ('Binfang Huang','img/xiaohuangren.jpeg');
+    insert into t_person(name, img_path) values ('KK','img/maidou.jpg');
+    insert into t_person(name, img_path) values ('Qiaorong Fan','img/huiguniang.jpg');
 
-insert into t_person(person_id, strike_count, start_count) values (1,2,3);
-insert into t_person(person_id, strike_count, start_count) values (2,1,1);
-insert into t_person(person_id, strike_count, start_count) values (3,2,2);
-insert into t_person(person_id, strike_count, start_count) values (4,2,2);
+    insert into t_strike(person_id, strike_count, star_count) values (1,2,3);
+    insert into t_strike(person_id, strike_count, star_count) values (2,1,1);
+    insert into t_strike(person_id, strike_count, star_count) values (3,2,2);
+    insert into t_strike(person_id, strike_count, star_count) values (4,2,2);
